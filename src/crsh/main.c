5fba72ddd58c1ec544fae665d24ad51df8e84af7
jiaranda <jiaranda@uc.cl>
2020-08-26 19:07:26 -0400

    first working code

--- src/crsh/main.c
+++ src/crsh/main.c
@@ -1,6 +1,11 @@
 #include "args/args.h"
 #include <stdio.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
 void run()
 {
@@ -12,6 +17,32 @@ void run()
     args_get(args);
 
     // Ejemplo de como utilizar el struct
+
+    pid_t childpid;
+    int retval;
+    int status;
+
+    printf("PARENT pid: %d\n", getpid());
+    childpid = fork();
+    printf("CHILD pid: %d\n", getpid());
+    
+    if (childpid == 0)
+    {
+      // execlp(args -> command, args -> command, args->argv[0], NULL);
+      if (strcmp(args -> command, "crexec")==0)
+      {
+        execvp(args -> argv[0], args -> argv);
+      }
+      exit(123);
+    }
+    else
+    {
+      printf("PARENT waiting for child...\n");
+      pid_t exited_child = wait(&status);
+      printf("PARENT: child's exit code: %d\n", WEXITSTATUS(status));
+    }
+
+
     printf("Command: %s\n", args->command);
     printf("Argc:    %i\n", args->argc);
     printf("Argv:    ");

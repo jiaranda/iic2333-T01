faf502a7061d90fc01f35a1d7d0e4aa49d78a0c6
jiaranda <jiaranda@uc.cl>
2020-08-30 22:06:22 -0400

    error message

--- src/crsh/main.c
+++ src/crsh/main.c
@@ -33,7 +33,7 @@ void run()
       if (strcmp(args -> command, "crexec")==0)
       {
         execvp(args -> argv[0], args -> argv);
-        printf("Error executing your command");
+        printf("Error executing your command\n");
         exit(0);
       }
       if (strcmp(args -> command, "crlist")==0)
